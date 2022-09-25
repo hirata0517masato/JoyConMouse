5a329ddcea5d758d08fc9ee068a4532a34289a68
hirata0517masato <32904561+hirata0517masato@users.noreply.github.com>
2022-09-26 00:39:47 +0900

    Soft_Update
    コードの更新
    ・感度調整
    ・中央ボタンおしながらジョイスティック操作で通常時よりもはやくマウス操作ができる

--- Soft/JoyConMouse/JoyConMouse.ino
+++ Soft/JoyConMouse/JoyConMouse.ino
@@ -8,16 +8,20 @@ int ShiftButtonPin = 7;
 int RightButtonPin = 8;
 int LeftButtonPin = 9;
 
-const int adjust = 100;
-const int adjust2 = 70;
+const int adjust = 150;
+
 const int adjustShift = 150;
+
 int yPos,xPos;
 int yIniPos,xIniPos;
 
 int ShiftPos = 0;
+int RPos = 0;
 
 int SbuttonState,LbuttonState,RbuttonState;
-  
+
+long long Xcnt = 0,Ycnt = 0;;
+
 void setup() {
   //Serial.begin(9600);
   Mouse.begin();
@@ -44,6 +48,7 @@ void loop() {
   yPos = analogRead(yPin) - yIniPos;
   xPos = analogRead(xPin) - xIniPos;
 
+  
   SbuttonState = digitalRead(ShiftButtonPin);
   LbuttonState = digitalRead(LeftButtonPin);
   RbuttonState = digitalRead(RightButtonPin);
@@ -60,24 +65,53 @@ void loop() {
       delay(50);
     }
     else{
-      if(abs(yPos) < 450 && abs(xPos) < 450)Mouse.move(0, max(min(yPos / adjust,3),-3), 0);
-      else Mouse.move(0, yPos / adjust2, 0);
-      delay(2);
+      if( 60 < abs(yPos) && abs(yPos) < adjust){
+        Ycnt++;
+        if(Ycnt > 50  || RbuttonState == LOW){
+          Ycnt = 0;
+          Mouse.move(0, (yPos > 0)?1 : -1, 0);
+        }
+      }else{
+        Ycnt++;
+        if(Ycnt > 6  || RbuttonState == LOW){
+          Ycnt = 0;
+          Mouse.move(0, yPos / adjust  , 0);
+        }
+      }
     }
+  }else{
+    Ycnt = 0;
   }
+
   if (xPos != 0) {
     if(ShiftPos == 1){
       Mouse.move(0, 0,-xPos / adjustShift);
       delay(50);
     }
-    else{
-      if(abs(yPos) < 450 && abs(xPos) < 450)Mouse.move(max(min(xPos / adjust,3),-3), 0, 0);
-      else Mouse.move(xPos / adjust2, 0, 0);
-      delay(2);
+    else{ 
+      if( 60 < abs(xPos) && abs(xPos) < adjust){
+        Xcnt++;
+        if( Xcnt > 50 || RbuttonState == LOW){
+          Xcnt = 0;
+          Mouse.move((xPos > 0)?1 : -1,0, 0);
+        }
+      }else{
+        Xcnt++;
+        if( Xcnt > 6 || RbuttonState == LOW){
+          Xcnt = 0;
+          Mouse.move(xPos / adjust , 0, 0);
+        }
+      }
     }
+  }else{
+    Xcnt = 0;
   }
 
- 
+  if(abs(xPos) > 100 || abs(yPos) > 100){
+    RPos = 2;
+  }
+  
+  delay(1);
 
   //Serial.println(yPos);
 
@@ -103,6 +137,8 @@ void loop() {
     }else if(Mouse.isPressed(MOUSE_LEFT)){
        Mouse.release(MOUSE_LEFT);
     }
+
+    /*
     if (RbuttonState == LOW) {
       if(!Mouse.isPressed(MOUSE_RIGHT)){
         Mouse.press(MOUSE_RIGHT);
@@ -110,5 +146,18 @@ void loop() {
     }else if(Mouse.isPressed(MOUSE_RIGHT)){
       Mouse.release(MOUSE_RIGHT);
     }
+    */
+    if (RbuttonState == LOW) {
+      if(RPos == 0)RPos = 1;
+    }else{
+      if(RPos == 1){
+        RPos = 0;
+        Mouse.press(MOUSE_RIGHT);
+        Mouse.release(MOUSE_RIGHT);
+      }else if(RPos == 2){
+        RPos = 0;
+      }
+    }
+    
   }
 }
